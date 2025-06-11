e565bcbf6e6c1efec093f6537033e10193c78ae4
ThaisaWerner2 <thaisawerner@gmail.com>
2025-06-11 15:36:15 -0300

    Setting display component

--- calculator/src/br/com/cod3r/calc/vision/Display.java
+++ calculator/src/br/com/cod3r/calc/vision/Display.java
@@ -1,11 +1,21 @@
 package br.com.cod3r.calc.vision;
 
 import javax.swing.*;
+import java.awt.*;
 
 // JPanel is a Swing component that can be used to create a panel for the calculator display.
 public class Display extends JPanel {
 
-    public Display() {
+    private final JLabel label; // JLabel is a Swing component that can be used to display text or images.
 
+    public Display() {
+        setBackground(new Color(46, 49, 50));
+        label = new JLabel("0");
+        label.setForeground(Color.WHITE);
+        label.setFont(new Font("Courier", Font.PLAIN, 24));
+
+        setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 25));
+
+        add(label);
     }
 }
