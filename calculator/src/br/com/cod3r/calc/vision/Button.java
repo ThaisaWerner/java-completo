0c64bff880f5add3b3b89f7a93be9475adce9b40
ThaisaWerner2 <thaisawerner@gmail.com>
2025-06-11 16:07:12 -0300

    Setting button component

--- calculator/src/br/com/cod3r/calc/vision/Button.java
+++ calculator/src/br/com/cod3r/calc/vision/Button.java
@@ -1,6 +1,16 @@
 package br.com.cod3r.calc.vision;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Button extends JButton {
+
+    public Button(String text, Color color) {
+        setText(text);
+        setFont(new Font("Courier", Font.PLAIN, 24));
+        setOpaque(true);
+        setBackground(color);
+        setForeground(Color.GRAY);
+        setBorder(BorderFactory.createLineBorder(Color.GRAY));
+    }
 }
--- calculator/src/br/com/cod3r/calc/vision/Keyboard.java
+++ calculator/src/br/com/cod3r/calc/vision/Keyboard.java
@@ -1,9 +1,14 @@
 package br.com.cod3r.calc.vision;
 
 import javax.swing.*;
+import java.awt.*;
 
 // JPanel is a Swing component that can be used to create a panel for the calculator keyboard.
 public class Keyboard extends JPanel {
+
+    private final Color DARK_PINK = new Color(255, 175, 204);
+    private final Color LIGHT_PINK = new Color(255, 200, 221);
+    private final Color PURPLE = new Color(205, 180, 219);
 
     public Keyboard() {
     }
