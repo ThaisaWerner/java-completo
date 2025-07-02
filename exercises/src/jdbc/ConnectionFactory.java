5470b9bfbc19b436c5f3b057c301fd5d2a9cda26
ThaisaWerner <thaisawerner@gmail.com>
2025-07-02 11:54:42 -0300

    Refactoring connection factory to connection.properties

--- exercises/src/jdbc/ConnectionFactory.java
+++ exercises/src/jdbc/ConnectionFactory.java
@@ -1,21 +1,31 @@
 package jdbc;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionFactory {
 
     public static Connection getConnection() {
         try {
-            String url = "jdbc:mysql://localhost:3307/java_course?verifyServerCertificate=false&useSSL=true";
-            String user = "root";
-            String password = "@Thai1995";
+            Properties properties = getProperties();
+            String url = properties.getProperty("database.url");
+            String user = properties.getProperty("database.user");
+            String password = properties.getProperty("database.password");
 
             //Connecting to the database
             return DriverManager.getConnection(url, user, password);
-        } catch (SQLException e) {
+        } catch (SQLException | IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static Properties getProperties() throws IOException {
+        Properties properties = new Properties();
+        String path = "/connection.properties";
+        properties.load(ConnectionFactory.class.getResourceAsStream(path));
+        return properties;
     }
 }
