2b09025383863f0ea8a2a10f25eb715a298375a6
ThaisaWerner <thaisawerner@gmail.com>
2025-07-08 12:21:40 -0300

    Deleting an user

--- exercises-jpa/src/test/basic/DeleteUser.java
+++ exercises-jpa/src/test/basic/DeleteUser.java
@@ -6,24 +6,19 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-public class UpdateUser3 {
+public class DeleteUser {
     public static void main(String[] args) {
 
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("exercises-jpa");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
 
-        entityManager.getTransaction().begin();
+        User user = entityManager.find(User.class, 8L);
 
-        User user = entityManager.find(User.class, 7L);
-        //Change object status to unmanaged
-        entityManager.detach(user);
-
-        user.setName("Leonardo");
-        user.setEmail("leonardo@lanche.com.br");
-
-        entityManager.merge(user);
-
-        entityManager.getTransaction().commit();
+        if(user != null) {
+            entityManager.getTransaction().begin();
+            entityManager.remove(user);
+            entityManager.getTransaction().commit();
+        }
 
         entityManager.close();
         entityManagerFactory.close();
