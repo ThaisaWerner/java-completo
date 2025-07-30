6c759cacd96b4b9539a6227334ce2ce379090b45
ThaisaWerner2 <thaisawerner@gmail.com>
2025-07-30 17:58:22 -0300

    Creating spring boot project

---
+++ exercices-sb/src/main/java/br/com/cod3r/exercices_sb/ExercicesSbApplication.java
@@ -0,0 +1,13 @@
+package br.com.cod3r.exercices_sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicesSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicesSbApplication.class, args);
+	}
+
+}
