c810b212ba6440916d718a4fa56650cfc88ac54d
ThaisaWerner2 <thaisawerner@gmail.com>
2025-10-07 14:04:16 -0300

    feat: creating login screen

--- exercises-javafx/src/fxml/AppFXML.java
+++ exercises-javafx/src/fxml/AppFXML.java
@@ -1,12 +1,26 @@
 package fxml;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+
+import java.net.URL;
 
 public class AppFXML extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        // Loading FXML file
+        URL fxmlFile = getClass().getResource("/fxml/Login.fxml");
+        GridPane root = FXMLLoader.load(fxmlFile);
+
+        Scene scene = new Scene(root, 350, 400);
+
+        primaryStage.setResizable(false);
+        primaryStage.setTitle("Login");
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
