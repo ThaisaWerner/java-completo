de45d7645de18595bc5562a8a2ebd1f15fecf953
ThaisaWerner2 <thaisawerner@gmail.com>
2025-10-08 13:29:39 -0300

    feat: using controlsFX (notification)

--- exercises-javafx/src/fxml/LoginController.java
+++ exercises-javafx/src/fxml/LoginController.java
@@ -1,8 +1,10 @@
 package fxml;
 
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import org.controlsfx.control.Notifications;
 
 public class LoginController {
 
@@ -13,7 +15,21 @@ public class LoginController {
     private PasswordField passwordField;
 
     public void login() {
-        System.out.println(emailField.getText());
-        System.out.println(passwordField.getText());
+        boolean validEmail = emailField.getText().equals("aluno@cod3r.com.br");
+        boolean validPassword = passwordField.getText().equals("123456");
+
+        if(validEmail && validPassword) {
+            Notifications.create()
+                    .position(Pos.TOP_RIGHT)
+                    .title("Login")
+                    .text("Login successful!")
+                    .showInformation();
+        } else {
+            Notifications.create()
+                    .position(Pos.TOP_RIGHT)
+                    .title("Login")
+                    .text("Invalid email or password!")
+                    .showError();
+        }
     }
 }
--- exercises-javafx/src/module-info.java
+++ exercises-javafx/src/module-info.java
@@ -2,6 +2,7 @@ module exercisesfx {
     requires javafx.controls;
     requires java.desktop;
     requires javafx.fxml;
+    requires org.controlsfx.controls;
 
     opens basic;
     opens layout;
